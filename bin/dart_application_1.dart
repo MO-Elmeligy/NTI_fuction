df22f5ad5aaf99ce582346e6f90896f19f7e021e
MO-Elmeligy <wasdmagneto17@gmail.com>
2025-05-27 13:06:25 +0300

    sum fuction

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,12 +1,15 @@
 import 'dart:io';
 
 void main() {
-  print("Enter a number:");
-  String? input = stdin.readLineSync();
-  int? number = int.tryParse(input ?? '');
-  if (number != null) {
-    print('You entered: $number');
-  } else {
-    print('Invalid input.');
-  }
+  print("Enter a first number:");
+  double first_number = double.parse(stdin.readLineSync()!);
+  print("Enter a scand number:");
+  double scand_number = double.parse(stdin.readLineSync()!);
+
+double res = sum(first_number, scand_number)!;
+  print( "Sum of $first_number and $scand_number is: $res");
 }
+
+  double? sum (double first_number, double scand_number) {
+  return first_number + scand_number;
+  }
