38acdafeee083d8022e6fc7f1e17feab225da1c9
MO-Elmeligy <wasdmagneto17@gmail.com>
2025-05-29 00:45:30 +0300

    Sumfunc

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,15 +1,16 @@
-import 'dart:io';
-
-void main() {
-  print("Enter a first number:");
-  double first_number = double.parse(stdin.readLineSync()!);
-  print("Enter a scand number:");
-  double scand_number = double.parse(stdin.readLineSync()!);
-
-double res = sum(first_number, scand_number)!;
-  print( "Sum of $first_number and $scand_number is: $res");
+import'dart:io';
+int Sumfunc(int n){
+    int sum=0;
+    while(n >0){
+        sum += n%10;
+        n=n~/10;
+    }
+    return sum;
 }
-
-  double? sum (double first_number, double scand_number) {
-  return first_number + scand_number;
-  }
+////////////////////////////////
+void main(){
+    stdout.write("enter you number pls : \n");
+    int number=int.parse(stdin.readLineSync()!);
+    print("The sum of that number is : ${Sumfunc(number)} \n");
+    
+}
