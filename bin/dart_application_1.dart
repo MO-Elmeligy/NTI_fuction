62436fcdb6751f0c21b390ecc6037060376e0168
MO-Elmeligy <wasdmagneto17@gmail.com>
2025-05-30 03:08:32 +0300

    isPalindrome

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,16 +1,25 @@
 import'dart:io';
-int Sumfunc(int n){
-    int sum=0;
-    while(n >0){
-        sum += n%10;
-        n=n~/10;
-    }
-    return sum;
+bool isPalindrome(String s) {
+  // قاعدة التوقف: إذا كانت السلسلة 0 أو 1 حرف، فهي Palindrome
+  if (s.length <= 1) {
+    return true;
+  }
+
+  // إذا أول وآخر حرف غير متساويين → ليست Palindrome
+  if (s[0] != s[s.length - 1]) {
+    return false;
+  }
+
+  // استدعاء الدالة على الجزء الأوسط من السلسلة
+  return isPalindrome(s.substring(1, s.length - 1));
 }
+
 ////////////////////////////////
-void main(){
-    stdout.write("enter you number pls : \n");
-    int number=int.parse(stdin.readLineSync()!);
-    print("The sum of that number is : ${Sumfunc(number)} \n");
-    
+void main() {
+  print(isPalindrome("level"));     // true
+  print(isPalindrome("radar"));     // true
+  print(isPalindrome("hello"));     // false
+  print(isPalindrome("madam"));     // true
+  print(isPalindrome("racecar"));   // true
+  
 }
