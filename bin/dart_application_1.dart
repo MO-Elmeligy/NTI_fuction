d4eff4e064b4d78422e79ee18e5d274f3e7f945c
MO-Elmeligy <wasdmagneto17@gmail.com>
2025-05-31 21:26:51 +0300

    divisors

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,25 +1,11 @@
-import'dart:io';
-bool isPalindrome(String s) {
-  // قاعدة التوقف: إذا كانت السلسلة 0 أو 1 حرف، فهي Palindrome
-  if (s.length <= 1) {
-    return true;
-  }
+import 'dart:io';
 
-  // إذا أول وآخر حرف غير متساويين → ليست Palindrome
-  if (s[0] != s[s.length - 1]) {
-    return false;
-  }
-
-  // استدعاء الدالة على الجزء الأوسط من السلسلة
-  return isPalindrome(s.substring(1, s.length - 1));
-}
-
-////////////////////////////////
 void main() {
-  print(isPalindrome("level"));     // true
-  print(isPalindrome("radar"));     // true
-  print(isPalindrome("hello"));     // false
-  print(isPalindrome("madam"));     // true
-  print(isPalindrome("racecar"));   // true
-  
+  stdout.write("Please choose a number: ");
+  int number = int.parse(stdin.readLineSync()!);
+  for (var i = 1; i <= number; i++) {
+    if (number % i == 0) {
+      print(i);
+    }
+  }
 }
